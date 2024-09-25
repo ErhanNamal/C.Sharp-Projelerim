187d9cf305b14b167d5e006dc3e27930c8de4ebb
ErhanNamal <13gunpara@gmail.com>
2024-09-25 16:29:41 +0300

    Add files via upload

---
+++ ödev/ConsoleApp10/Program.cs
@@ -0,0 +1,13 @@
+﻿int sayi = 0;
+int toplam = 0;
+Console.WriteLine("Devam etmek için bir tuşa basınız (Çıkış için ESC ye basınız)");
+
+while (true)
+{
+    Console.Write("Eklenecek sayıyı giriniz =>");
+    if (Console.ReadKey().Key == ConsoleKey.Escape)
+    sayi =Int32.Parse(Console.ReadLine());
+    toplam += sayi;
+    
+}
+Console.WriteLine("şimdiye kadar girdiğiniz sayıların toplamı => {0}",toplam);
