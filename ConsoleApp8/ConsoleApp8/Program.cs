6e2936ec721c0287e8e455186c1f2c00e3e6a46e
ErhanNamal <13gunpara@gmail.com>
2024-09-25 12:09:27 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿int bas =0;
+int bit =0;
+int toplam=0;
+
+Console.Write("Başlangıç Sayısını Giriniz=");
+bas =Convert.ToInt32(Console.ReadLine());
+if (bas % 2 == 0)
+{
+    Console.Write("Bitiş Sayısını Giriniz=");
+    bit = Convert.ToInt32(Console.ReadLine());
+
+    for (int sayac = bas; sayac <= bit; sayac += 2)
+    {
+        toplam = sayac + toplam;
+    }
+    Console.WriteLine(toplam);
+}
+else
+{
+    Console.WriteLine("Başlangıcı çift giriniz");
+}
