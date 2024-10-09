32e4b70c4b2420deb2e7e46f0096271f04cd0d97
ErhanNamal <13gunpara@gmail.com>
2024-10-09 10:27:58 +0300

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,42 @@
+﻿double s1 = 0;double s2 = 0;double sonuc; string islem2;
+
+ekranbilgi();
+hangislem();
+sGir();
+s1 = sGir();
+s2 = sGir();
+dortislemmetodu($"{islem2}");
+ekranaYaz();
+
+
+void hangislem()
+{
+	Console.WriteLine("Lütfen bir islem seçiniz(+,-,/,*)=>");
+	islem2 = Console.ReadLine();
+}
+void ekranbilgi()
+{
+    Console.WriteLine("4 işlem Uygulaması");
+}
+void dortislemmetodu(string islem)
+{
+	switch (islem)
+	{
+		case "+": sonuc = s1 + s2; break;
+		case "-": sonuc = s1 - s2; break;
+		case "*": sonuc = s1 * s2; break;
+		case "/": sonuc = s1 / s2; break;
+
+		default:Console.WriteLine("Hatalı Giriş Yaptınız");
+			break;
+	}
+}
+double sGir()
+{
+	Console.Write("Sayıyı Giriniz=>");
+	return Convert.ToDouble(Console.ReadLine());
+}
+void ekranaYaz()
+{
+	Console.WriteLine();
+}
