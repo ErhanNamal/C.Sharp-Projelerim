47e00bc09e5b7b250363438549825cf1823c7269
ErhanNamal <13gunpara@gmail.com>
2024-10-16 13:25:21 +0300

    Update Program.cs

--- H4Ödev/H4Ödev/Program.cs
+++ H4Ödev/H4Ödev/Program.cs
@@ -1,79 +1,75 @@
-﻿Random random = new Random();
-int sayi = 1;
-bool tekrar = false;
+bool kontrol = false;
 int[] randomsayi = new int[10];
 int[] tahmin = new int[10];
-randomsayi[0] = random.Next(1, 21);
-//int dtahmin = 0;
-//int hak = 5;
 
-while (sayi < 10)
+
+Randomsayıuret();
+sayigiris(5,0);
+Ekranayazdır();
+
+void Ekranayazdır()
 {
-    int secsayi = random.Next(1, 21);
-    bool ekleme = true;
-
-    foreach (int psecsayi in randomsayi)
+    if (kontrol)
     {
-        if (psecsayi == secsayi) { ekleme = false; break; }
+        Console.WriteLine("Kaybettiniz! Haklarınız bitti.");
     }
-
-    if (ekleme)
+    else
     {
-        randomsayi[sayi] = secsayi;
-        sayi++;
+        Console.WriteLine("Kazandınız! Tebrikler!");
     }
 }
 
-for (int i = 0; i < 5; i++)
+void Hakkontrol(int kalanHak)
 {
-    Console.Write($"Girilen {i + 1}. Sayı => ");
-    int kulgiris = Convert.ToInt32(Console.ReadLine());
-
-    foreach (int sayi2 in randomsayi)
+    if (kalanHak == 0)
     {
-        if (sayi2 == kulgiris)
+        Console.WriteLine("Haklarınız bitti!");
+    }
+    else
+    {
+        Console.WriteLine($"{kalanHak} kadar hakkınız kaldı.");
+    }
+}
+
+void sayigiris(int hak,int i)
+{
+    for (i = 0; i < hak; i++)
+    {
+        Console.Write("Sayı Giriniz => ");
+        int kulgiris = Convert.ToInt32(Console.ReadLine());
+
+        foreach (int sayi2 in randomsayi)
         {
-            tekrar = true;
-            break;
+            if (sayi2 == kulgiris)
+            {
+                kontrol = true;
+                break;
+            }
+            
+        }
+        if (kontrol) break;
+    }
+    Hakkontrol(hak - i);
+}
+
+void Randomsayıuret()
+{
+    Random random = new Random();
+    int sayi = 0;
+    while (sayi < 10)
+    {
+        int secsayi = random.Next(1, 21);
+        bool ekleme = true;
+
+        foreach (int psecsayi in randomsayi)
+        {
+            if (psecsayi == secsayi) { ekleme = false; break; }
+        }
+
+        if (ekleme)
+        {
+            randomsayi[sayi] = secsayi;
+            sayi++;
         }
     }
-
-    if (tekrar) break;
 }
-
-if (tekrar) { Console.Write("Kaybettiniz :d"); }
-else { Console.Write("Kazandınız :D"); }
-/*Console.WriteLine("1-20 arasında 10 adet sayı tahmin edin. ");
-while (hak > 0)
-{
-    Console.WriteLine($"Kalan Hakkınız =>{hak}");
-    for (int i = 0; i < 10; i++)
-    {
-        Console.Write($"{i+1}. sayıyı tahmin edin =>");
-        tahmin[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    //kontrol tahmin
-    for (int i = 0; i < 10; i++)
-    {
-        if (randomsayi[i] == tahmin[i])
-        {
-            dtahmin++;
-        }
-    }
-    Console.WriteLine($"Doğru tahmin sayısı=>{dtahmin}");
-    hak--;
-    if (dtahmin == 10)
-    {
-        Console.WriteLine("Tebrikler Kazandınız :D");
-        break;
-    }
-}
-if (hak== 0)
-{
-    Console.WriteLine("Hakkınız Bitti, Kaybettiniz Cevap=>");
-    foreach (var item in randomsayi)
-    {
-        Console.Write($"{sayi}");
-    }
-}
-*/
