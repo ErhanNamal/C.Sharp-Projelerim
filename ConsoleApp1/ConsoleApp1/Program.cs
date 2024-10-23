dcd6725c1d4c50874401d8effb0d9ab5decc2981
ErhanNamal <13gunpara@gmail.com>
2024-10-23 09:07:49 +0300

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,155 @@
-﻿Console.BackgroundColor = ConsoleColor.Black;
-Console.ForegroundColor = ConsoleColor.Green;
-int a;
-int b;
-a = 20;
-b = 300;
-/*.Write(a*b);
-Console.WriteLine(" m2");
-Console.Write("Çevresi=");
-*/
-Console.WriteLine("Alanı=" + (a * b) + " metrekare(m2)");
-Console.WriteLine();
-/*
-Console.Write(2*(a+b));
-Console.WriteLine(" m");
-*/
-Console.WriteLine("Çevresi=" + (2*(a + b)) + " metre(m)");
+int sinir;
+int seviye;
+int mayinsayisi;
+
+Dictionary<int, bool> dizi = new Dictionary<int, bool>();
+
+zorlukSec();
+List<int> mayinlar = mayinlariYerlestir(seviye, sinir, mayinsayisi);
+Ciz(sinir, seviye, dizi);
+if (adimAt(sinir, seviye, mayinlar))
+{
+    MesajYaz("\t\t   oyun Bitti   ", ConsoleColor.Red);
+}
+else
+{
+    MesajYaz("\t\t   bravo   ", ConsoleColor.DarkGreen);
+}
+void zorlukSec()
+{
+    int zorluk = 0;
+    Console.Clear();
+    MesajYaz("\tKolay Seviye İçin 1 e Basınız.", ConsoleColor.Green);
+    MesajYaz("\tNormal Seviye İçin 2 e Basınız.", ConsoleColor.Blue);
+    MesajYaz("\tZor Seviye İçin 3 e Basınız.", ConsoleColor.Red);
+    MesajYaz("\tYukarıdan Bir Zorluk Seçiniz", ConsoleColor.Black);
+    zorluk = Convert.ToInt32(Console.ReadLine());
+
+    switch (zorluk)
+    {
+        case 1:
+            sinir = 20;
+            seviye = 4;
+            mayinsayisi = 20;
+            break;
+        case 2:
+            sinir = 30;
+            seviye = 5;
+            mayinsayisi = 40;
+            break;
+        case 3:
+            sinir = 40;
+            seviye = 6;
+            mayinsayisi = 60;
+            break;
+        default:
+            Console.WriteLine("Lütfen geçerli bir rakam giriniz.");
+            zorlukSec();
+            break;
+    }
+}
+
+bool adimAt(int _sinir, int _seviye, List<int> _mayinlar)
+{
+    int sayi;
+    int basamakSay = 0;
+    bool mayin = false;
+    for (int i = 1; i <= _sinir; i = i + _seviye)
+    {
+        sayi = sayiGir(i, i + _seviye - 1);
+        if (sayi == _mayinlar[basamakSay])
+        {
+            dizi.Add(sayi, true);
+            mayin = true;
+        }
+        else
+        {
+            dizi.Add(sayi, false);
+        }
+        basamakSay++;
+        Ciz(_sinir, _seviye, dizi);
+        if (mayin) { break; }
+    }
+    return mayin;
+}
+
+int sayiGir(int min, int max)
+{
+    int sayi = 0;
+    while (true)
+    {
+        Console.Write($"Sıradaki Adımınızı Atınız({min} - {max}):=>");
+        string girilen = Console.ReadLine();
+        if (Int32.TryParse(girilen, out sayi))
+        {
+            if (sayi >= min && sayi <= max) { break; }
+            else
+            {
+                MesajYaz("Hatalı Giriş-Aralık İhlali", ConsoleColor.Red);
+            }
+        }
+        else
+        {
+            MesajYaz("Hatalı Giriş-Sayısal değerler gir", ConsoleColor.Red);
+        }
+    }
+    return sayi;
+}
+
+void MesajYaz(string v, ConsoleColor renk)
+{
+    Console.BackgroundColor = renk;
+    Console.WriteLine(v.ToUpper());
+    Console.BackgroundColor = ConsoleColor.Black;
+}
+
+List<int> mayinlariYerlestir(int seviye, int sinir)
+{
+    List<int> mayinlar = new List<int>();
+    Random r = new Random();
+    for (int i = 1; i <= sinir; i = i + seviye)
+    {
+        mayinlar.Add(r.Next(i, i + seviye));
+    }
+
+    //mayinlar.ForEach(x =>Console.WriteLine(x));
+    return mayinlar;
+}
+
+void Ciz(int _sinir, int _seviye, Dictionary<int, bool> _dizi)
+{
+    Console.Clear();
+    ConsoleColor a_renk = ConsoleColor.Black;
+    ConsoleColor o_renk = ConsoleColor.White;
+    Console.WriteLine("\t\t BAŞLANGIÇ");
+    Console.WriteLine("\t----------------------------");
+    for (int i = 1; i <= _sinir; i++)
+    {
+        if (_dizi.Keys.ToList().IndexOf(i) == -1)
+        {
+            a_renk = ConsoleColor.Black;
+            o_renk = ConsoleColor.White;
+        }
+        else
+        {
+            if (_dizi[i])
+            {
+                a_renk = ConsoleColor.Red;
+            }
+            else
+            {
+                a_renk = ConsoleColor.Green;
+                o_renk = ConsoleColor.Black;
+            }
+        }
+        Console.BackgroundColor = a_renk;
+        Console.ForegroundColor = o_renk;
+
+        Console.Write($"\t  {i}  ");
+        if (i % _seviye == 0) Console.WriteLine();
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    Console.WriteLine("\t----------------------------");
+    Console.WriteLine("\t\t BİTİS");
+}
